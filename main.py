f9baec93c4834adda948d6d6b4698eb8a4e92cbb
Derekabc <chendondon19930606@gmail.com>
2020-08-26 20:57:02 -0400

    clean_up

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import argparse
 import os
 from datetime import datetime
 
+
 def parse_args():
     default_config_dir = 'configs'
     plot_dir = 'results/'
@@ -31,16 +32,17 @@ def parse_args():
     return args
 
 
-def main(args, DER_num, lines_num, loads_num, DER_controller, sampling_time = 0.1, disturbance=True):
+def main(args, DER_num, lines_num, loads_num, DER_controller, sampling_time=0.1, plotting=False, disturbance=True):
     # time points
     start = datetime.now()
     x = []
     t_pre = np.linspace(0, sampling_time * 10, 11)
     t = np.linspace(0, sampling_time * 30, 31)
     if args.mode == 'Vnom':
-        x_pre = odeint(DER_controller.VSI_VFctrl_func, x0, t_pre, atol=1e-10, rtol=1e-11, mxstep=5000, printmessg=True)
+        x_pre = odeint(DER_controller.VSI_VFctrl_func, x0, t_pre, atol=1e-10, rtol=1e-11, mxstep=5000, printmessg=False)
     else:
-        x_pre = odeint(DER_controller.VSI_VFctrl_func, x_critic, t_pre, atol=1e-10, rtol=1e-11, mxstep=5000, printmessg=True)
+        x_pre = odeint(DER_controller.VSI_VFctrl_func, x_critic, t_pre, atol=1e-10, rtol=1e-11, mxstep=5000,
+                       printmessg=False)
 
     x1 = (x_pre[-1]).tolist()
     x.append(x_pre.tolist())
@@ -55,11 +57,15 @@ def main(args, DER_num, lines_num, loads_num, DER_controller, sampling_time = 0.
             DER_controller.disturbance_L = np.random.rand(DER_num) * 0
         steps = 11 + step
         if args.mode == 'Vnom':
-            x_af = odeint(DER_controller.VSI_VFctrl_func, x1, np.array([(steps - 1) * sampling_time, steps * sampling_time]), atol=1e-10, rtol=1e-11, mxstep=5000, printmessg=True)
+            x_af = odeint(DER_controller.VSI_VFctrl_func, x1,
+                          np.array([(steps - 1) * sampling_time, steps * sampling_time]), atol=1e-10, rtol=1e-11,
+                          mxstep=5000, printmessg=False)
             x = x + [x_af[-1].tolist()]
             x1 = (x_af[-1]).tolist()
         else:
-            x_af = odeint(DER_controller.VSI_VFctrl_func, x1, np.array([(steps - 1) * sampling_time, steps * sampling_time]), atol=1e-10, rtol=1e-11, mxstep=5000, printmessg=True)
+            x_af = odeint(DER_controller.VSI_VFctrl_func, x1,
+                          np.array([(steps - 1) * sampling_time, steps * sampling_time]), atol=1e-10, rtol=1e-11,
+                          mxstep=5000, printmessg=False)
             x = x + [x_af[-1].tolist()]
             x1 = (x_af[-1]).tolist()
     print(datetime.now() - start)
@@ -108,77 +114,78 @@ def main(args, DER_num, lines_num, loads_num, DER_controller, sampling_time = 0.
                 reward += - np.abs(1 - vi)
     print(reward)
 
-    # # subplot: https://matplotlib.org/3.1.3/gallery/pyplots/pyplot_scales.html#sphx-glr-gallery-pyplots-pyplot-scales-py
-    # plt.figure()
-    #
-    # plt.subplot(221)
-    # for a in range(DER_num):
-    #     plt.plot(t, w[a], label='DER_id %s' % (a + 1))
-    # # plt.xlim(0, 6)
-    # plt.xlabel("time")
-    # # plt.legend()
-    # plt.title("DER Frequency")
-    #
-    # plt.subplot(222)
-    # plt.xlabel("time")
-    # plt.ylabel("ratio")
-    # plt.title("Active power ratio")
-    # for b in range(DER_num):
-    #     plt.plot(t, PDG[b], label='DER_id %s' % (b + 1))
-    # # plt.legend()
-    # # plt.xlim(0, 6)
-    #
-    # plt.subplot(223)
-    # plt.xlabel("time")
-    # plt.ylabel("voltage")
-    # plt.title("DER Voltage")
-    # for c in range(DER_num):
-    #     plt.plot(t, vbus[c], label='DER_id %s' % (c + 1))
-    # # plt.legend()
-    # # plt.xlim(0, 6)
-    #
-    #
-    # plt.subplot(224)
-    # plt.xlabel("time")
-    # plt.ylabel("ratio")
-    # plt.title("Reactive Power Ratio")
-    # for d in range(DER_num):
-    #     plt.plot(t, QDG[d], label='DER_id %s' % (d + 1))
-    # # plt.legend()
-    # # plt.xlim(0, 6)
-    #
-    # plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
-    #                     wspace=0.35)
-    #
-    # plt.show()
-    # plt.savefig(args.plot_dir + 'DER_' + str(args.num_DER) + '.png')
-    #
-    # plt.xlabel("time")
-    # plt.ylabel("voltage")
-    # plt.title("DER Voltage")
-    # for c in range(len(vbus)):
-    #     plt.plot(t, vbus[c], label='DER_id %s' % (c + 1))
-    # # plt.legend()
-    # # plt.xlim(0, 6)
-    # plt.show()
-    #
-    # plt.xlabel("time")
-    # plt.ylabel("Input wn")
-    # plt.title("Secondary frequency Control Input")
-    # for e in range(DER_num):
-    #     plt.plot(t, wn[e], label='DER_id %s' % (e + 1))
-    # # plt.legend()
-    # # plt.xlim(0, 6)
-    # plt.show()
-    #
-    # plt.xlabel("time")
-    # plt.ylabel("Input vn")
-    # plt.title("Secondary Voltage Control Input")
-    # for f in range(DER_num):
-    #     plt.plot(t, vn[f], label='DER_id %s' % (f + 1))
-    # # plt.legend()
-    # # plt.xlim(0, 6)
-    # plt.show()
+    if plotting:
+        # subplot: https://matplotlib.org/3.1.3/gallery/pyplots/pyplot_scales.html#sphx-glr-gallery-pyplots-pyplot-scales-py
+        plt.figure()
+
+        plt.subplot(221)
+        for a in range(DER_num):
+            plt.plot(t, w[a], label='DER_id %s' % (a + 1))
+        # plt.xlim(0, 6)
+        plt.xlabel("time")
+        # plt.legend()
+        plt.title("DER Frequency")
+
+        plt.subplot(222)
+        plt.xlabel("time")
+        plt.ylabel("ratio")
+        plt.title("Active power ratio")
+        for b in range(DER_num):
+            plt.plot(t, PDG[b], label='DER_id %s' % (b + 1))
+        # plt.legend()
+        # plt.xlim(0, 6)
+
+        plt.subplot(223)
+        plt.xlabel("time")
+        plt.ylabel("voltage")
+        plt.title("DER Voltage")
+        for c in range(DER_num):
+            plt.plot(t, vbus[c], label='DER_id %s' % (c + 1))
+        # plt.legend()
+        # plt.xlim(0, 6)
+
+
+        plt.subplot(224)
+        plt.xlabel("time")
+        plt.ylabel("ratio")
+        plt.title("Reactive Power Ratio")
+        for d in range(DER_num):
+            plt.plot(t, QDG[d], label='DER_id %s' % (d + 1))
+        # plt.legend()
+        # plt.xlim(0, 6)
+
+        plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                            wspace=0.35)
+
+        plt.show()
+        plt.savefig(args.plot_dir + 'DER_' + str(args.num_DER) + '.png')
+
+        plt.xlabel("time")
+        plt.ylabel("voltage")
+        plt.title("DER Voltage")
+        for c in range(len(vbus)):
+            plt.plot(t, vbus[c], label='DER_id %s' % (c + 1))
+        # plt.legend()
+        # plt.xlim(0, 6)
+        plt.show()
+
+        plt.xlabel("time")
+        plt.ylabel("Input wn")
+        plt.title("Secondary frequency Control Input")
+        for e in range(DER_num):
+            plt.plot(t, wn[e], label='DER_id %s' % (e + 1))
+        # plt.legend()
+        # plt.xlim(0, 6)
+        plt.show()
+
+        plt.xlabel("time")
+        plt.ylabel("Input vn")
+        plt.title("Secondary Voltage Control Input")
+        for f in range(DER_num):
+            plt.plot(t, vn[f], label='DER_id %s' % (f + 1))
+        # plt.legend()
+        # plt.xlim(0, 6)
+        plt.show()
 
     return reward
 
@@ -186,8 +193,10 @@ def main(args, DER_num, lines_num, loads_num, DER_controller, sampling_time = 0.
 if __name__ == '__main__':
     args = parse_args()
     os.makedirs(args.plot_dir, exist_ok=True)
-    num_test = 10
+    num_test = 1
     random_seed = 0
+    # Flag for plotting
+    plot = True
     reward_list = []
     if args.num_DER == 4:
         from configs.parameters_4 import *
@@ -203,7 +212,9 @@ if __name__ == '__main__':
         random_seed += 1
         der_controller = DER_controller(args.mode, args.critic_bus_id, DER_num, lines_num, loads_num, DER_dic, BUSES,
                                         BUS_LOAD, rline, Lline, a_ctrl, AP,
-                                        G, Vnom, wref, mp1, rN, wc, F, wb, Lf, Cf, rLf, Lc, rLc, kp, ki,  sampling_time=sampling_time)
-        reward = main(args, DER_num, lines_num, loads_num, der_controller, sampling_time, disturbance=False)  # False True
+                                        G, Vnom, wref, mp1, rN, wc, F, wb, Lf, Cf, rLf, Lc, rLc, kp, ki,
+                                        sampling_time=sampling_time)
+        reward = main(args, DER_num, lines_num, loads_num, der_controller, sampling_time, plotting=plot,
+                      disturbance=False)  # False True
         reward_list.append(reward)
     print(np.mean(reward_list))
